a351a227b29fa2f7a43872d0d1f91931eaea5053
Barboza <thiago.barboza@accenture.com>
2017-04-20 08:29:18 -0300

    metodo edicao

--- src/pages/editar/editar.ts
+++ src/pages/editar/editar.ts
@@ -1,6 +1,9 @@
-import { Inventario } from './../../services/inventario';
+import { Resposta } from './../../services/resposta';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Inventario } from './../../services/inventario';
+import { InventarioService } from './../../services/inventario.service';
 
 /**
  * Generated class for the Editar page.
@@ -16,13 +19,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class EditarPage {
   
   item: Inventario;
+  res: Resposta;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private service: InventarioService) {
     this.item = navParams.get("item");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Editar');
+  }
+
+  onSalvar() {
+    this.service.atualizar(this.item)
+                .subscribe(
+                  resp => this.res = resp,
+                  err => this.res = err,
+                  () => console.log('finalizado atualização')
+                );
+  }
+
+  onCancelar() {
+    this.navCtrl.pop();
   }
 
 }
